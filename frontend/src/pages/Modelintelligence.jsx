a045d12248bb6efe1b88934713efa5984771a9ef
parthmalhotra-tech <parth07092007malhotra@gmail.com>
2026-09-09 23:06:12 +0530

    backend connected to frontend

--- frontend/src/pages/Modelintelligence.jsx
+++ frontend/src/pages/Modelintelligence.jsx
@@ -18,7 +18,7 @@ import {
 /* BACKEND CONFIG */
 /* ------------------------------------------------ */
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 /* ------------------------------------------------ */
 /* MODEL INFORMATION */
